41e9ceb8c666f9a498949bec16a5e9d4c332c801
Preston <prestonl13@nycstudents.net>
2025-04-10 19:31:06 +0000

    moved photos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import React from 'react';
-import {Gallery} from './Gallery.js';
-import './App.css';
-import SearchBar from './SearchBar.js';
-import { useState } from 'react';
-import ImageSlider from './ImageSlider.js';
+import "./App.css";
+import React from "react";
+import {Gallery} from "./Gallery.js";
+import "./App.css";
+import SearchBar from "./SearchBar.js";
+import { useState } from "react";
+import ImageSlider from "./ImageSlider.js";
 
 function App() {
 
   const slides = [
-    {url: 'https://media.cnn.com/api/v1/images/stellar/prod/230515101146-the-weeknd-121222.jpg?c=original', title: 'The Weeknd'},
-    {url: 'https://i8.amplience.net/i/naras/bruno-mars_MI0004141313-MN0001032082', title: 'Bruno Mars'},
-    {url: 'https://c.files.bbci.co.uk/048c/live/2bb03f70-1753-11ef-b507-edbcd7518f5c.jpg', title: 'Billie Eillish'},
-    {url: 'https://variety.com/wp-content/uploads/2017/11/kendrick-lamar-variety-hitmakers.jpg', title: 'Kendrick Lamar'},
-    {url: 'https://slpecho.com/wp-content/uploads/2024/10/sabrina-carpenter-061724-tout-9f62f8da05e544e3a8333d032fb05faa-1200x801.jpg', title: 'Sabrina Carpenter'},
+    {url: "http://localhost:3000/theweeknd.jpg", title: "The Weeknd"},
+    {url: "http://localhost:3000/bruno.jpg", title: "Bruno Mars"},
+    {url: "http://localhost:3000/billie.jpg", title: "Billie Eillish"},
+    {url: "http://localhost:3000/kendrick.jpg", title: "Kendrick Lamar"},
+    {url: "http://localhost:3000/sabrina.jpg", title: "Sabrina Carpenter"},
   ];
 
   const containerStyles = {
-    width: '500px',
-    height: '280px',
-    margin: '0 auto',
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
   };
 
   const [inputText, setInputText] = useState("");
